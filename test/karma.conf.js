33b5a57afe32de18d192d9faccc49ff286b608c5
chieffancypants <chieffancypants@gmail.com>
2014-03-02 16:17:38 -0500

    package and bower updates

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       '../bower_components/angular/angular.js',
-      '../bower_components/angular-animate/angular-animate.js',
       '../bower_components/angular-mocks/angular-mocks.js',
       '../bower_components/mousetrap/mousetrap.js',
       '../bower_components/mousetrap/tests/libs/key-event.js',
