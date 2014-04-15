a5fb8ced2be13192ac2fcf3ab9e8ff6e0b5d820d
joshbowdoin <josh.bowdoin@athletic.net>
2014-04-15 15:45:13 -0700

    Update Hotkeys to work with ui-router
    This is a start on ui-router compatibility.  ```$stateChangeSuccess``` is not called on initial page load, so any shortcuts defined on the first route would not be registered.  I'm not sure how to get around this.

--- src/hotkeys.js
+++ src/hotkeys.js
@@ -136,29 +136,33 @@
 
 
       $rootScope.$on('$routeChangeSuccess', function (event, route) {
-        purgeHotkeys();
-
-        if (route.hotkeys) {
-          angular.forEach(route.hotkeys, function (hotkey) {
-            // a string was given, which implies this is a function that is to be
-            // $eval()'d within that controller's scope
-            // TODO: hotkey here is super confusing.  sometimes a function (that gets turned into an array), sometimes a string
-            var callback = hotkey[2];
-            if (typeof(callback) === 'string' || callback instanceof String) {
-              hotkey[2] = [callback, route];
-            }
-
-            // todo: perform check to make sure not already defined:
-            // this came from a route, so it's likely not meant to be persistent:
-            hotkey[4] = false;
-            _add.apply(this, hotkey);
-          });
-        }
+          _routeChanged(route);
       });
-      
+
       $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-          purgeHotkeys();
+          _routeChanged(toState);
       });
+
+      function _routeChanged(route) {
+          purgeHotkeys();
+
+          if (route.hotkeys) {
+              angular.forEach(route.hotkeys, function (hotkey) {
+                  // a string was given, which implies this is a function that is to be
+                  // $eval()'d within that controller's scope
+                  // TODO: hotkey here is super confusing.  sometimes a function (that gets turned into an array), sometimes a string
+                  var callback = hotkey[2];
+                  if (typeof (callback) === 'string' || callback instanceof String) {
+                      hotkey[2] = [callback, route];
+                  }
+
+                  // todo: perform check to make sure not already defined:
+                  // this came from a route, so it's likely not meant to be persistent:
+                  hotkey[4] = false;
+                  _add.apply(this, hotkey);
+              });
+          }
+      }
 
 
       // Auto-create a help menu:
