e99455d434ee8990509a0dc6900001ca70cd38e3
Michał Prokopowicz <michael.prokopowicz@gmail.com>
2015-06-19 23:41:20 +0200

    performance: cache results of Hotkey.format

--- src/hotkeys.js
+++ src/hotkeys.js
@@ -134,6 +134,7 @@
         this.action = action;
         this.allowIn = allowIn;
         this.persistent = persistent;
+        this._formated = null;
       }
 
       /**
@@ -145,17 +146,19 @@
        * TODO: this gets called a lot.  We should cache the result
        */
       Hotkey.prototype.format = function() {
+        if(this._formated === null) {
+          // Don't show all the possible key combos, just the first one.  Not sure
+          // of usecase here, so open a ticket if my assumptions are wrong
+          var combo = this.combo[0];
 
-        // Don't show all the possible key combos, just the first one.  Not sure
-        // of usecase here, so open a ticket if my assumptions are wrong
-        var combo = this.combo[0];
-
-        var sequence = combo.split(/[\s]/);
-        for (var i = 0; i < sequence.length; i++) {
-          sequence[i] = symbolize(sequence[i]);
+          var sequence = combo.split(/[\s]/);
+          for (var i = 0; i < sequence.length; i++) {
+            sequence[i] = symbolize(sequence[i]);
+          }
+          this._formated = sequence;
         }
 
-        return sequence;
+        return this._formated;
       };
 
       /**
